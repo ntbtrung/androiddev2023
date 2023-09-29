da14dfb23af33533a61dd55ba15c48b37bd6e389
ntbtrung <trungntb.bi11-275@st.usth.edu.vn>
2023-09-29 15:41:10 +0700

    attaching api into news page

---
+++ mobileproject/app/src/main/java/com/example/mobileproject/ModelClass.java
@@ -0,0 +1,63 @@
+package com.example.mobileproject;
+
+public class ModelClass {
+
+    private String author,title,description,url,urlToImage,publishedAt;
+
+    public ModelClass(String author, String title, String description, String url, String urlToImage, String publishedAt) {
+        this.author = author;
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.urlToImage = urlToImage;
+        this.publishedAt = publishedAt;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUrlToImage() {
+        return urlToImage;
+    }
+
+    public String getPublishedAt() {
+        return publishedAt;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setUrlToImage(String urlToImage) {
+        this.urlToImage = urlToImage;
+    }
+
+    public void setPublishedAt(String publishedAt) {
+        this.publishedAt = publishedAt;
+    }
+}
---
+++ mobileproject/app/src/main/java/com/example/mobileproject/PagerAdapter.java
@@ -0,0 +1,40 @@
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.mobileproject.latestnewsFragment;
+
+public class PagerAdapter extends FragmentPagerAdapter {
+
+    int tabcount;
+
+    public PagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+        tabcount = behavior;
+    }
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position){
+
+            case 0:
+                return new latestnewsFragment();
+
+            case 1:
+                return new twitterFragment();
+
+            case 2:
+                return new facebookFragment();
+
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return tabcount;
+    }
+}
